4f8eab0798b3a097f9c7102e3a7e90c8fb612d38
reza2boyce <reza2boyce@gmail.com>
2022-05-27 01:09:53 +0430

    get button element data in callback function

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,18 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Hello World !');
+    }
+
+    const handleClickmeAgain = (name, e) => {
+        console.log(name , ' ' , e.target);
+    }
+
     return (
         <>
             <div className="container">
                 <h2>This is Home page</h2>
+                <button onClick={handleClick} >Click me</button>
+                <button onClick={(e) => handleClickmeAgain('Hello', e)}>Click me agian</button>
             </div>
         </>
     )
