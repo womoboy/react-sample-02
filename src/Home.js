b502d5e8e8470f4cd6e65bc612123f41d261c24d
reza2boyce <reza2boyce@gmail.com>
2022-05-27 18:10:49 +0430

    create bloglist component for reusable cod

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,18 @@
 import { useState } from 'react';
+import Bloglist from './Bloglist';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body: 'descreption 1', author: 'Aria', id: 1},
         {title: 'Welcome party', body: 'descreption 2', author: 'Babak', id: 2},
-        {title: 'Web dev top tips', body: 'descreption 3', author: 'Ata', id: 3}
+        {title: 'Web dev top tips', body: 'descreption 3', author: 'Aria', id: 3}
     ]);
     
     return (
         <>
             <div className="home">
-                {blogs.map((blog) => (
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>writtn by : {blog.author}</p>
-                    </div>
-                ))}
+                <Bloglist blogs={blogs} title='All Blogs!' />
+                <Bloglist blogs={blogs.filter((blog) => blog.author === 'Aria')} title='Aria Blogs!' />
             </div>
         </>
     )
