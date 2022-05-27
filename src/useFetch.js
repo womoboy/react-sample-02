9bb17f46f104bce82fa0f4c31c6b07ff7781675a
reza2boyce <reza2boyce@gmail.com>
2022-05-28 02:26:15 +0430

    fetch aborted error managed

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("Sorry , we can't find data!");
@@ -19,10 +21,15 @@ const useFetch = (url) => {
           setIsPending(false);
         })
         .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log('Fetch aborted!');
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+    return () => abortCont.abort();
   }, [url]);
   return { data, isPending, error };
 };
